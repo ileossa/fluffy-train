3424e6979dd2ad7fb5e5c22c07dfcb5e35b26aba
Vincent L <vincent-externe.lafosse@edelia.fr>
2019-07-22 19:15:54 +0200

    :tada: initial commit

---
+++ src/main/java/Boostrap.java
@@ -0,0 +1,6 @@
+public class Boostrap {
+
+    public static void main(String[] args){
+        System.out.println("hello cracking coding interview");
+    }
+}
---
+++ src/test/java/BoostrapTest.java
@@ -0,0 +1,14 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+
+/**
+ * This is my own implementation from exercices from cracking coding
+ * Each problem is
+ */
+class BoostrapTest {
+
+    public static void main(String[] args){
+        System.out.println("Welcom to The Cracking coding challenge \nRoad to 80K");
+    }
+
+}
