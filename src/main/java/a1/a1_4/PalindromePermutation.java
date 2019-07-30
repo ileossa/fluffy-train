49c3d9cb9b0bf4163cefb589fcaf46c197de94c3
ileossa <4510253+ileossa@users.noreply.github.com>
2019-07-30 22:26:06 +0200

    :sparkles: start 1.4 excercice ( palindrome)

---
+++ src/main/java/a1/a1_4/PalindromePermutation.java
@@ -0,0 +1,16 @@
+package a1.a1_4;
+
+/**
+ * Palindrome Permutation
+ * Given a string, write a function to check if it is a permutation of a palindrome. A palindrome is a word or phrase
+ * that is the same forward and backwards. A permutation is a rearrangement of letters. The palindrome does not need to
+ * be limited to just dictionary words.
+ * Example
+ * Input: KAYAK
+ * Output: TRUE
+ */
+public class PalindromePermutation {
+    public boolean check(String kayak) {
+        return false;
+    }
+}
