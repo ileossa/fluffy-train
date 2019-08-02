b8ab7f6b2fc09c9ce3dd21f1fbf5d175fb35e698
Vincent L <vincent-externe.lafosse@edelia.fr>
2019-08-02 15:29:05 +0200

    :construction: wip: i need fix challenge 1.2

--- src/main/java/a1/a1_5/OneAway.java
+++ src/main/java/a1/a1_5/OneAway.java
@@ -1,5 +1,9 @@
 package a1.a1_5;
 
+import java.util.stream.IntStream;
+
+import static java.lang.Boolean.*;
+
 /**
  * There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a
  * character. Given two strings, write a function to check if they are one edit(or zero edits) aways.
@@ -11,4 +15,43 @@ package a1.a1_5;
  * pale,  bake    -> false
  */
 public class OneAway {
+
+
+    public boolean check(String input, String output) {
+
+        char[] inputChars = input.toCharArray();
+        char[] outputChars = output.toCharArray();
+
+        if(inputChars.length == outputChars.length)
+        {
+            System.out.println("meme taille");
+            sameLenght(inputChars, outputChars);
+        }
+        else if( inputChars.length > outputChars.length){
+            System.out.println("input plus grand");
+        }else{
+            System.out.println("output plus grand");
+        }
+
+
+//        if () return Boolean.FALSE;
+
+        return TRUE;
+    }
+
+
+
+    protected boolean sameLenght(char[] inputChars, char[] outputChars) {
+        for (int i = 0; i < inputChars.length; i++) {
+            System.out.println(inputChars[i]+" == "+outputChars[i]);
+            if(inputChars[i] != outputChars[i]){
+                System.out.println(inputChars[i]+" != "+outputChars[i+1]);
+                if(inputChars[i] != outputChars[++i]){
+                    System.out.println(inputChars[i]+" 1!= "+outputChars[i+1]);
+                    return true;
+                }
+            }
+        }
+        return TRUE;
+    }
 }
--- src/test/java/a1/a1_5/OneAwayTest.java
+++ src/test/java/a1/a1_5/OneAwayTest.java
@@ -1,9 +1,9 @@
 package a1.a1_5;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.DisplayNameGeneration;
-import org.junit.jupiter.api.DisplayNameGenerator;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
+
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 /**
  * There are three types of edits that can be performed on strings: insert a character, remove a character, or replace a
@@ -20,23 +20,47 @@ import org.junit.jupiter.api.Test;
 @DisplayName("One away challenge")
 class OneAwayTest {
 
+    static OneAway oneAway;
+
+    @BeforeAll
+    public static void init(){
+        oneAway = new OneAway();
+    }
+
     @Test
     void should_return_true_with_input_pale_output_ple() {
-
+        assertTrue(oneAway.check("pale", "ple"));
     }
 
     @Test
     void should_return_true_with_input_pales_output_pale() {
-
+        assertTrue(oneAway.check("pales", "pale"));
     }
 
     @Test
     void should_return_true_with_input_pale_output_bale() {
+        assertTrue(oneAway.check("pale", "bale"));
     }
 
 
     @Test
     void should_return_false_with_input_pale_output_bake() {
+        assertFalse(oneAway.check("pale", "bake"));
+    }
+
+    @Nested
+    @DisplayName("private method to check two strings with same lenght")
+    class sameLength {
+
+        @Test
+        void should_succes_with_pale_pale(){
+            assertTrue(oneAway.sameLenght("pale".toCharArray(), "pale".toCharArray()));
+        }
+
+        @Test
+        void should_fail_with_two_differents_letters(){
+            assertFalse(oneAway.sameLenght("pale".toCharArray(), "babe".toCharArray()));
+        }
     }
 
 }
